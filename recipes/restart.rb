66521a51d5003b5d91648e64ae453f4223d90f65
Ryan Schlesinger <ryan@instanceinc.com>
2013-07-05 21:31:45 -0700

    Delay execute with notification

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,10 +1,14 @@
 node[:deploy].each do |application, deploy|
   execute 'restart sidekiq' do
-    command 'env > /tmp/env; /usr/bin/god restart workers'
+    command '/usr/bin/god restart workers'
     environment({
       'PATH' => '/usr/local/bin:/usr/bin'
     })
     user 'root'
-    action :run
+    action :nothing
+  end
+
+  ruby_block 'restart sidekiq later' do
+    notifies :run, resources(:execute => 'restart sidekiq'), :delayed
   end
 end
