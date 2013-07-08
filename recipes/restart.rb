e811fe3b98948acdb8f337c62ad9b422a5f27f46
Ryan Schlesinger <ryan@instanceinc.com>
2013-07-08 14:51:09 -0700

    Sleep to wait for god to be ready

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,6 +1,7 @@
 node[:deploy].each do |application, deploy|
   bash 'restart sidekiq' do
     code <<-EOH
+    sleep 1
     /usr/bin/god restart workers
     EOH
     user 'root'
--- recipes/stop.rb
+++ recipes/stop.rb
@@ -1,6 +1,7 @@
 node[:deploy].each do |application, deploy|
   bash 'stop sidekiq' do
     code <<-EOH
+    sleep 1
     /usr/bin/god stop workers
     EOH
     user 'root'
