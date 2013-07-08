b44ccf0b350ad6658a98737a47849a98e02558e8
Ryan Schlesinger <ryan@instanceinc.com>
2013-07-08 14:00:15 -0700

    Remove delayed notifications

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,20 +1,9 @@
 node[:deploy].each do |application, deploy|
   bash 'restart sidekiq' do
     code <<-EOH
-    sleep 1
     /usr/bin/god restart workers
     EOH
-    #environment({
-    #  'PATH' => '/usr/local/bin:/usr/bin:/bin'
-    #})
     user 'root'
-    action :nothing
-  end
-
-  ruby_block 'restart sidekiq later' do
-    block do
-      true
-    end
-    notifies :run, resources(:bash => 'restart sidekiq'), :delayed
+    action :run
   end
 end
--- recipes/stop.rb
+++ recipes/stop.rb
@@ -1,9 +1,8 @@
 node[:deploy].each do |application, deploy|
-  execute 'stop sidekiq' do
-    command 'env > /tmp/env; /usr/bin/god stop workers'
-    environment({
-      'PATH' => '/usr/local/bin:/usr/bin'
-    })
+  bash 'stop sidekiq' do
+    code <<-EOH
+    /usr/bin/god stop workers
+    EOH
     user 'root'
     action :run
   end
