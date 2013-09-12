70d7d02caac2a5b7982e201e92fd09bb19160796
Ryan Schlesinger <ryan@instanceinc.com>
2013-09-12 10:15:11 -0700

    Sidekiq loads config/sidekiq.yml from deploy path

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ node[:deploy].each do |application, deploy|
       :add_to_path => "/usr/local/bin",
       :uid => deploy[:user],
       :dir => File.join(deploy[:deploy_to], 'current'),
-      :start => "cd #{File.join(deploy[:deploy_to], 'current')}; bundle exec sidekiq -e #{deploy[:rails_env]} -P #{pid_file} 2>&1 | logger -t sidekiq",
+      :start => "cd #{File.join(deploy[:deploy_to], 'current')}; bundle exec sidekiq -C config/sidekiq.yml -e #{deploy[:rails_env]} -P #{pid_file} 2>&1 | logger -t sidekiq",
       :stop => "cd #{File.join(deploy[:deploy_to], 'current')}; bundle exec sidekiqctl stop #{pid_file} 60",
       :memory_max => (1434 * 1024)
     })
